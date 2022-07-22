2047320af823c142550a2659caced59f9dca5faa
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-22 20:45:20 +0200

    Used yodas and saved phone info

--- main.py
+++ main.py
@@ -1,70 +1,51 @@
+from yodas import Menu, Yoda
+import os
+
+
+def choosePhone():
+    jsons = []
+    dataDir = config.contents()["phoneInfoDir"]
+    for file in os.listdir(dataDir):
+        if file.endswith(".txt"):
+            jsons.append(file)
+    print(jsons)
+
+
+def calculateProfits():
+    dataDir = config.contents()["phoneInfoDir"]
+    name = dataDir + input("Input the phone model: ")
+    phone = Yoda(name, ["sellPrice", "pricePhone", "priceBattery", "priceScreen", "priceChassis", "priceOther"])
+    contents = phone.contents()
+    losses = float(contents["pricePhone"]) + \
+             float(contents["priceBattery"]) + \
+             float(contents["priceScreen"]) + \
+             float(contents["priceChassis"]) + \
+             float(contents["priceOther"])
+    profit = float(contents["sellPrice"]) - losses
+    print("\n\tCost: €{0}\n\tSell: €{1}\n\tProfit: €{2}\n".format(losses,
+                                                                  float(contents["pricePhone"]),
+                                                                  profit))
+
+
 def calculateBid():
-    specificQuestions = False
-    # currency: value in euros
-    exchangeRate = {"€": 1.00,
-                    "£": 1.17}
-
-    costToRepair = 0
-    if specificQuestions:
-        itemList = ["battery", "screen", "chassis"]
-        for item in itemList:
-            costToRepair += getValidatedInput("What is the price of a new {0}?: €".format(item))
-        costToRepair += getValidatedInput("What is the shipping price: €")
-    else:
-        costToRepair = getValidatedInput("What is the total cost to repair (incl shipping)?: €")
-
-    salePrice = getValidatedInput("What is the sale price: €")
-    desiredProfit = getValidatedInput("What is your desired profit: €")
-
-    maxPrice = salePrice - (desiredProfit + costToRepair)
-    vat = 1.21
-
-    print("\nFor a profit of €{0}".format(round(desiredProfit)))
-    print("VAT incl.\tVAT excl.")
-    for currency in exchangeRate:
-        localCurrency = maxPrice / exchangeRate[currency]
-        localCurrencyVat = round(localCurrency * (1 / vat))
-        price = "{0} {1}\t\t{0} {2}".format(currency, round(localCurrency), localCurrencyVat)
-        print(price)
-
-
-def calculateProfit():
-    losses = getValidatedInput("What is the final sale price including shipping?: €")
-    losses += getValidatedInput("What is the total cost to repair?: €")
-    salePrice = getValidatedInput("What is the estimated sale price?: €")
-    print("\nYour profit is €{0}".format(round(salePrice - losses, 2)))
-
-
-def getValidatedInput(question):
-    while True:
-        try:
-            return float(input(question))
-        except ValueError:
-            print("That doesn't look like a good number")
-
-
-def menu(options):
-    print("")
-    print("=" * 50)
-    print("0: Exit")
-    for i in range(len(options)):
-        print("{0}: {1}".format(i + 1, options[i]))
-    chosenOption = int(getValidatedInput("Choose an option: "))
-    if chosenOption == 0:
-        exit()
-    print("=" * 50)
-    print("")
-    return chosenOption - 1
+    print("Hello 2")
 
 
 def main():
-    menuOptions = ["Calculate bid price", "Calculate profit"]
-    chosenOption = menu(menuOptions)
-    if chosenOption == 0:
-        calculateBid()
-    elif chosenOption == 1:
-        calculateProfit()
+    # Make sure the data dir finishes '/'
+    dataDir = config.contents()["phoneInfoDir"]
+    if not dataDir.endswith("/"):
+        contents = config.contents()
+        contents["phoneInfoDir"] = dataDir + "/"
+        config.write(contents)
+
+    # Start menu
+    menu = Menu([exit, calculateProfits, calculateBid])
+    while True:
+        menu.select()
 
 
 if __name__ == '__main__':
+    # create config
+    config = Yoda("config.json", ["phoneInfoDir"])
     main()
